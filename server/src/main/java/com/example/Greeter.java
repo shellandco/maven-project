aef72a087a1cbac18a6774699c1f97ef2809acd2
Nicolas HAHANG <nicolas.hahang@msc.com>
2019-06-19 09:03:29 +0200

    Wed 19 Jun 2019 09:03:28 fixing warning using jenkins checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param someone my name
+   * @return greeting string nicoh
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
